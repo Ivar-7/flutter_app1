9d0b6e95aa161a6169f10cae843d045c4e661cc0
Ivar-7 <leank6071@gmail.com>
2024-06-22 10:59:55 +0300

    goodbye cruel world

--- lib/gradient_containter.dart
+++ lib/gradient_containter.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app1/styled_text.dart';
+
+var startAlignment = Alignment.topLeft;
+var endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
@@ -6,23 +10,18 @@ class GradientContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [
+          colors: const [
             Color.fromARGB(255, 165, 36, 161),
             Color.fromARGB(255, 214, 133, 219),
             Color.fromARGB(217, 244, 232, 244)
           ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+          begin: startAlignment,
+          end: endAlignment,
         ),
       ),
-      child: const Center(
-        child: Text(
-          'Hello World',
-          style: TextStyle(color: Colors.white, fontSize: 24),
-        ),
-      ),
+      child: const Center(child: StyledText('GOODBYE CRUEL WORLD!')),
     );
   }
 }
