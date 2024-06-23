e79ea2d54088f8c4f08d9109748a3e184098f072
Ivar-7 <leank6071@gmail.com>
2024-06-23 10:18:46 +0300

    added images

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,9 @@ import 'package:flutter_app1/gradient_containter.dart';
 
 void main() {
   const colors = [
-            Color.fromARGB(255, 165, 36, 161),
-            Color.fromARGB(255, 214, 133, 219),
-            Color.fromARGB(217, 244, 232, 244)
+    Color.fromARGB(255, 165, 36, 161),
+    Color.fromARGB(255, 214, 133, 219),
+    Color.fromARGB(217, 244, 232, 244)
   ];
   runApp(
     const MaterialApp(
