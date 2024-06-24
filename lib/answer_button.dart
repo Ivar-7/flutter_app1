b55078eb53b2ff9cca5cc878134fff85c7cc3698
Ivar-7 <leank6071@gmail.com>
2024-06-24 08:32:11 +0300

    answer button refactor

---
+++ lib/answer_button.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget {
+  const AnswerButton(this.answer, this.ontap, {super.key});
+
+  final String answer;
+  final void Function() ontap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ElevatedButton(
+        onPressed: ontap,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: const Color.fromARGB(255, 66, 4, 77),
+          foregroundColor: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(40),
+          ),
+        ),
+        child: Text(answer),
+      ),
+    );
+  }
+}
--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app1/answer_button.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -8,8 +9,37 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+
+  void saveAnswer() {}
+
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Questions Screen'));
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'The Questions...',
+            style: TextStyle(color: Colors.white),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          AnswerButton(
+            'constraints to ensure uniformity. Here are two possible solutions:',
+            saveAnswer,
+          ),
+          AnswerButton(
+            'answer',
+            saveAnswer,
+          ),
+          AnswerButton(
+            'answer 3',
+            saveAnswer,
+          ),
+        ],
+      ),
+    );
   }
 }
